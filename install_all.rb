0a3e9d9cbc5cd2ea7eeb0609fded48783a0045f5
Guillaume Claret <dev@clarus.me>
2015-11-11 11:42:38 +0100

    install_all.rb added

---
+++ install_all.rb
@@ -0,0 +1,16 @@
+require 'json'
+
+version = ARGV[0]
+if version.nil?
+  puts("One argument expected (a Coq version).")
+else
+  puts("Computing the maximal installation for Coq #{version}.")
+  puts("Be sure to start from an OPAM where nothing is installed.")
+  system("opam install --criteria=\"+new,-notuptodate\" --show-action --json=installs.json coq.#{version} >/dev/null")
+
+  packages = JSON.parse(File.read("installs.json"))
+  packages = packages[0].map {|package| package["install"]}
+  packages = packages.find_all {|package| package["name"].include?("coq")}
+  packages = packages.map {|package| "#{package["name"]}.#{package["version"]}"}.join(" ")
+  puts("opam install #{packages}")
+end
